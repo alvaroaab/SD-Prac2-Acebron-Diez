317aeaf1792113d9ee31308cc99d7a415d7b0c16
Álvaro <alvaro.acebron@estudiants.urv.cat>
2019-05-26 11:03:45 +0200

    Prova llista de llistes

--- prac2.py
+++ prac2.py
@@ -7,35 +7,42 @@ def leader_func(x):
 
 	channel = connection.channel()
 	channel.declare(queue='leader')
-	slave_list = [0,2,1,3]
+	slave_list = []
 
 	for i in range(x):
 		method, properties, body = channel.basic_get('leader')
 		channel.basic_ack(delivery_tag = method.delivery_tag)
-		channel.queue_declare(queue = body)
-		channel.basic_publish(exchange = '', routing_key = body, body = str(x))
 		slave_list.append(body)
 
 	for i in range(len(slave_list)):
 		index = random.randint(0, len(slave_list)-1)
 		selected_slave = slave_list.pop(index)
 		channel.queue_declare(queue = selected_slave)
-		channel.basic_publish(exchange = '', routing_key = selected_slave, body = '')
+		channel.basic_publish(exchange = '', routing_key = selected_slave, body = 'Selected')
+	return
 
 def slave_func(x):
+	id_slave = x[0]
+	num_maps = x[1]
+
+	return id_slave
 
 
-pw_leader = pywren.ibm_cf_executor(rabbitmq_monitor=True)
+#pw_leader = pywren.ibm_cf_executor(rabbitmq_monitor=True)
 
-num_maps = input("Introdueixi el numero de slaves")
-ids= list(range(0,num_maps))
+num_maps = int(input("Introdueixi el numero de slaves: "))
+data = []
+for i in range(num_maps):
+	data.append([i,num_maps])
 
-pw_leader.call_async(leader_func,num_maps)
+print(data)
+
+#pw_leader.call_async(leader_func,num_maps)
 
 pw_slave = pywren.ibm_cf_executor(rabbitmq_monitor=True)
 pw_slave.map(slave_func, ids)
 
 print(pw_slave.get_result())
-pw_leader.clean()
+#pw_leader.clean()
 pw_slave.clean()
 
